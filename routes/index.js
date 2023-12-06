dad9fa3078b0837048ab47f8336b1ed15a3c7f33
DESKTOP-YASH <yashgandhi1410@gmail.com>
2023-12-05 22:44:33 -0500

    Adding views and routes for login and registration.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import authRoutes from './auth_routes.js';
+const constructorMethod = (app) => {
+    app.use(authRoutes);
+    //app.use('/users', userRoutes);
+    app.use('*', (req, res) => {
+        res.status(404).send("No such route exist.");
+    });
+};
+export default constructorMethod;
