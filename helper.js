ed2cad3abff50e87ea89f275b432b2772f09b6a3
Jinghao Huang <huang893040504@gmail.com>
2023-12-06 22:08:58 -0500

    update helper.js

--- helper.js
+++ helper.js
@@ -14,3 +14,39 @@ export const trimString = (arg) => {
     }
     return arg;
 }
+
+export const checkStr = (param, paramName) => {
+    if (!param) throw `${paramName} must be exist!`;
+    if (typeof param !== "string")
+      throw `the type of ${paramName} must be string!`;
+    param = param.trim();
+    if (param.length === 0)
+      throw `${paramName} cannot consist of spaces entirely!`;
+    return param;
+  };
+  export const checkDate = (date, whatDate) => {
+    date = checkString(date, whatDate);
+    if (
+      !date.match(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[0-1])\/[12][0-9]{3}$/gim)
+    )
+      throw `${whatDate} type is invalid`;
+    const [month, day, year] = date.split("/");
+    const formatDate = new Date(year, month - 1, day);
+    if (
+      !(
+        formatDate.getFullYear() === +year &&
+        formatDate.getMonth() + 1 === +month &&
+        formatDate.getDate() === +day
+      )
+    )
+      throw `${whatDate} is invalid`;
+    return date;
+  };
+  
+  export const checkName = (param, paramName) => {
+    param = checkString(param, paramName);
+    if (!param.match(/^[a-z]{0,10}( [a-z]{0,10}){1,2}$/gi))
+      throw `${paramName} must consist of letters and 2-3 spaces`;
+    return param;
+  };
+  
