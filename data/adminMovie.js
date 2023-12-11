1d4ff72dee4a2c0a4affb6bc61083f2dc94f06a3
Jinghao Huang <huang893040504@gmail.com>
2023-12-11 14:52:36 -0500

    Successful connection to login and admin

--- data/adminMovie.js
+++ data/adminMovie.js
@@ -30,6 +30,8 @@ export const getAll = async () => {
         director: movie.artists.director,
         actors: movie.artists.actors,
         genre: movie.genre,
+        overall_rating: movie.overall_rating,
+        contentRating: movie.contentRating,
       };
     })
     .filter((e) => e);
@@ -44,7 +46,7 @@ export const addNewMovie = async (
   actors,
   writer,
   producer,
-  ageRestriction,
+  contentRating,
   thumbnail
 ) => {
   title = checkStr(title, "title");
@@ -58,10 +60,9 @@ export const addNewMovie = async (
   });
   if (writer !== "") writer = checkName(writer, "writer");
   producer = checkName(producer, "producer");
-  if (ageRestriction === "false" || ageRestriction === "true")
-    ageRestriction = !!ageRestriction;
-  if (typeof ageRestriction !== "boolean")
-    throw "the type of ageRestriction must be boolean";
+  const allContentRatings = ["G", "PG", "PG-13", "R", "NC-17"];
+  if (!allContentRatings.includes(contentRating))
+    throw "contentRating is not valid";
   if (
     !thumbnail.match(
       /^\/uploads\/[0-9]{13}-[a-zA-Z_0-9]{0,20}\.(jpg|bmp|png)$/g
@@ -79,9 +80,10 @@ export const addNewMovie = async (
     genre,
     releaseDate,
     artists,
-    ageRestriction,
+    contentRating,
     reviews: [],
     thumbnail,
+    overall_rating: "0",
   };
   const moviesCollection = await movies();
   const insertInfo = await moviesCollection.insertOne(newMovie);
@@ -109,7 +111,7 @@ export const update = async (
   genre,
   releaseDate,
   artists,
-  ageRestriction,
+  contentRating,
   thumbnail
 ) => {
   const id = checkStr(movieId, "movieId");
@@ -118,8 +120,9 @@ export const update = async (
   releaseDate = checkDate(releaseDate, "releaseDate");
   if (typeof artists !== "object" || Array.isArray(artists))
     throw "the type of artists must be object";
-  if (typeof ageRestriction !== "boolean")
-    throw "the type of ageRestriction must be boolean";
+  const allContentRatings = ["G", "PG", "PG-13", "R", "NC-17"];
+  if (!allContentRatings.includes(contentRating))
+    throw "contentRating is not valid";
   const moviesCollection = await movies();
   const targetMovie = await moviesCollection.findOne({ _id: new ObjectId(id) });
   if (!targetMovie) throw "the movie does not exist";
@@ -127,7 +130,7 @@ export const update = async (
     {
       _id: new ObjectId(id),
     },
-    { $set: { title, genre, releaseDate, artists, ageRestriction, thumbnail } },
+    { $set: { title, genre, releaseDate, artists, contentRating, thumbnail } },
     { returnDocument: "after" }
   );
   if (!updateInfo) throw "update failed";
--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,7 @@ import {
   getMoviesByFlaggedTimes,
   removeByFlaggedTimes,
 } from "../data/flaggedReviews.js";
+import { isAdminAuthenticated, isAuthenticated } from "../middleware.js";
 
 export const adminMoviesRouter = Router();
 
@@ -27,7 +28,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-adminMoviesRouter.route("/").get(async (req, res) => {
+adminMoviesRouter.route("/").get(isAdminAuthenticated, async (req, res) => {
   try {
     const allMovies = await adminMovies.getAll();
     return res.render("admin", { allMovies });
@@ -37,7 +38,8 @@ adminMoviesRouter.route("/").get(async (req, res) => {
 });
 
 adminMoviesRouter
-  .get("/addMovie", async (req, res) => {
+  .route("/addMovie")
+  .get(isAdminAuthenticated, async (req, res) => {
     const allGenres = [
       "action",
       "adventure",
@@ -55,111 +57,127 @@ adminMoviesRouter
     ];
     res.render("uploadMovie", { allGenres });
   })
-  .post("/addMovie", upload.single("image"), async (req, res) => {
+  .post(
+    isAdminAuthenticated,
+    upload.single("image"),
+    async (req, res) => {
+      try {
+        const data = req.body;
+        if (!data) throw "input is required";
+        let {
+          title,
+          genre,
+          releaseDate,
+          director,
+          actors,
+          writer,
+          producer,
+          contentRating,
+        } = data;
+        const thumbnail = req.file ? "/uploads/" + req.file.filename : "";
+        title = checkStr(title, "title");
+        if (!Array.isArray(genre)) throw "the type of genre must be array";
+        releaseDate = checkDate(releaseDate, "releaseDate");
+        director = checkName(director, "director");
+        actors = checkStr(actors, "actors");
+        actors = actors.split(",")?.map((eachName) => {
+          eachName = checkName(eachName, eachName);
+          return eachName;
+        });
+        if (!Array.isArray(actors)) throw "the type of genre must be array";
+        if (writer !== "") writer = checkName(writer, "writer");
+        producer = checkName(producer, "producer");
+        const allContentRatings = ["G", "PG", "PG-13", "R", "NC-17"];
+        if (!allContentRatings.includes(contentRating))
+          throw "contentRating is not valid";
+        const newMovie = await adminMovies.addNewMovie(
+          title,
+          genre,
+          releaseDate,
+          director,
+          actors,
+          writer,
+          producer,
+          contentRating,
+          thumbnail
+        );
+        return res.redirect("/admin");
+      } catch (e) {
+        return res.status(400).json({ error: e.toString() });
+      }
+    }
+  );
+
+adminMoviesRouter
+  .route("/:movieId")
+  .delete(isAdminAuthenticated, async (req, res) => {
+    let id = req.params.movieId;
     try {
-      const data = req.body;
-      if (!data) throw "input is required";
-      let {
-        title,
-        genre,
-        releaseDate,
-        director,
-        actors,
-        writer,
-        producer,
-        ageRestriction,
-      } = data;
-      const thumbnail = req.file ? "/uploads/" + req.file.filename : "";
-      title = checkStr(title, "title");
-      if (!Array.isArray(genre)) throw "the type of genre must be array";
-      releaseDate = checkDate(releaseDate, "releaseDate");
-      director = checkName(director, "director");
-      actors = checkStr(actors, "actors");
-      actors = actors.split(",")?.map((eachName) => {
-        eachName = checkName(eachName, eachName);
-        return eachName;
-      });
-      if (!Array.isArray(actors)) throw "the type of genre must be array";
-      if (writer !== "") writer = checkName(writer, "writer");
-      producer = checkName(producer, "producer");
-      if (ageRestriction === "false" || ageRestriction === "true")
-        ageRestriction = !!ageRestriction;
-      if (typeof ageRestriction !== "boolean")
-        throw "the type of ageRestriction must be boolean";
-      const newMovie = await adminMovies.addNewMovie(
-        title,
-        genre,
-        releaseDate,
-        director,
-        actors,
-        writer,
-        producer,
-        ageRestriction,
-        thumbnail
-      );
-      return res.redirect("/admin");
+      id = checkStr(id, "movieId");
+      if (!ObjectId.isValid(id)) throw "the movieId is not valid";
     } catch (e) {
-      return res.status(400).json({ error: e.toString() });
+      return res.status(400).json({ error: e });
+    }
+    try {
+      const targetMovie = await adminMovies.getMovie(id);
+    } catch (e) {
+      return res.status(404).json({ error: "movie not found" });
+    }
+    try {
+      const targetMovie = await adminMovies.remove(id);
+      return res.json({ message: "movie deleted successfully" });
+    } catch (e) {
+      return res.status(404).json({ error: e });
     }
   });
 
-adminMoviesRouter.route("/:movieId").delete(async (req, res) => {
-  let id = req.params.movieId;
-  try {
-    id = checkStr(id, "movieId");
-    if (!ObjectId.isValid(id)) throw "the movieId is not valid";
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-  try {
-    const targetMovie = await adminMovies.getMovie(id);
-  } catch (e) {
-    return res.status(404).json({ error: "movie not found" });
-  }
-  try {
-    const targetMovie = await adminMovies.remove(id);
-    return res.json({ message: "movie deleted successfully" });
-  } catch (e) {
-    return res.status(404).json({ error: e });
-  }
-});
+adminMoviesRouter
+  .route("/update/:movieId")
+  .get(isAdminAuthenticated, async (req, res) => {
+    let movieId = req.params.movieId;
+    try {
+      movieId = checkStr(movieId, "movieId");
+      if (!ObjectId.isValid(movieId)) throw "the movieId is not valid";
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+    try {
+      const movie = await adminMovies.getMovie(movieId);
+      movie.ratingOption = {
+        G: movie.contentRating === "G",
+        PG: movie.contentRating === "PG",
+        PG13: movie.contentRating === "PG-13",
+        R: movie.contentRating === "R",
+        NC17: movie.contentRating === "NC-17",
+      };
+      const allGenres = [
+        "action",
+        "adventure",
+        "comedy",
+        "drama",
+        "fantasy",
+        "horror",
+        "musicals",
+        "mystery",
+        "romance",
+        "science fiction",
+        "sports",
+        "thriller",
+        "western",
+      ];
+      const genresWithSelection = allGenres.map((genre) => {
+        return { name: genre, selected: movie.genre.includes(genre) };
+      });
+      res.render("updateMovie", { movie, genresWithSelection });
+    } catch (error) {
+      res.status(404).send("Movie not found");
+    }
+  });
 
-adminMoviesRouter.get("/update/:movieId", async (req, res) => {
-  let movieId = req.params.movieId;
-  try {
-    movieId = checkStr(movieId, "movieId");
-    if (!ObjectId.isValid(movieId)) throw "the movieId is not valid";
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-  try {
-    const movie = await adminMovies.getMovie(movieId);
-    const allGenres = [
-      "action",
-      "adventure",
-      "comedy",
-      "drama",
-      "fantasy",
-      "horror",
-      "musicals",
-      "mystery",
-      "romance",
-      "science fiction",
-      "sports",
-      "thriller",
-      "western",
-    ];
-    const genresWithSelection = allGenres.map((genre) => {
-      return { name: genre, selected: movie.genre.includes(genre) };
-    });
-    res.render("updateMovie", { movie, genresWithSelection });
-  } catch (error) {
-    res.status(404).send("Movie not found");
-  }
-});
 adminMoviesRouter.post(
   "/toUpdate/:movieId",
   upload.single("image"),
+  isAdminAuthenticated,
   async (req, res) => {
     const movieId = req.params.movieId;
     const data = req.body;
@@ -184,7 +202,7 @@ adminMoviesRouter.post(
         actors,
         writer,
         producer,
-        ageRestriction,
+        contentRating,
       } = data;
 
       const thumbnail = req.file ? "/uploads/" + req.file.filename : "";
@@ -207,21 +225,16 @@ adminMoviesRouter.post(
       };
       if (typeof artists !== "object" || Array.isArray(artists))
         throw "the type of artists must be object";
-      ageRestriction =
-        ageRestriction === "true"
-          ? true
-          : ageRestriction === "false"
-          ? false
-          : ageRestriction;
-      if (typeof ageRestriction !== "boolean")
-        throw "the type of ageRestriction must be boolean";
+      const allContentRatings = ["G", "PG", "PG-13", "R", "NC-17"];
+      if (!allContentRatings.includes(contentRating))
+        throw "contentRating is not valid";
       const newMovie = await adminMovies.update(
         id,
         title,
         genre,
         releaseDate,
         artists,
-        ageRestriction,
+        contentRating,
         thumbnail
       );
       if (newMovie) return res.redirect("/admin");
@@ -231,47 +244,53 @@ adminMoviesRouter.post(
   }
 );
 
-adminMoviesRouter.post("/addReview/:movieId", async (req, res) => {
-  const movieId = req.params.movieId;
-  const data = req.body;
-  const { userId, text, rating, ts, flaggedTimes } = data;
-  try {
-    const review = await createReviews(
-      movieId,
-      userId,
-      text,
-      rating,
-      ts,
-      flaggedTimes
-    );
-    return res.status(200).json("succeed");
-  } catch (e) {
-    return res.status(404).json({ error: e });
-  }
-});
+adminMoviesRouter
+  .route("/addReview/:movieId")
+  .post(isAdminAuthenticated, async (req, res) => {
+    const movieId = req.params.movieId;
+    const data = req.body;
+    const { userId, text, rating, ts, flaggedTimes } = data;
+    try {
+      const review = await createReviews(
+        movieId,
+        userId,
+        text,
+        rating,
+        ts,
+        flaggedTimes
+      );
+      return res.status(200).json("succeed");
+    } catch (e) {
+      return res.status(404).json({ error: e });
+    }
+  });
 
-adminMoviesRouter.get("/flaggedReviews", async (req, res) => {
-  try {
-    const allFlaggedReviews = await getMoviesByFlaggedTimes();
-    return res.render("flaggedReviews", { allFlaggedReviews });
-  } catch (e) {
-    return res.status(404).json({ error: e });
-  }
-});
+adminMoviesRouter
+  .route("/flaggedReviews")
+  .get(isAdminAuthenticated, async (req, res) => {
+    try {
+      const allFlaggedReviews = await getMoviesByFlaggedTimes();
+      return res.render("flaggedReviews", { allFlaggedReviews });
+    } catch (e) {
+      return res.status(404).json({ error: e });
+    }
+  });
 
-adminMoviesRouter.delete("/flaggedReviews/:reviewId", async (req, res) => {
-  let reviewId = req.params.reviewId;
-  try {
-    reviewId = checkStr(reviewId, "reviewId");
-    if (!ObjectId.isValid(reviewId)) throw "the reviewId is not valid";
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-  try {
-    const deleteInfo = await removeByFlaggedTimes(reviewId);
-    if (!deleteInfo) throw "deleting failed";
-    return res.render("flaggedReviews");
-  } catch (e) {
-    return res.status(404).json({ error: e });
-  }
-});
+adminMoviesRouter
+  .route("/flaggedReviews/:reviewId")
+  .delete(isAdminAuthenticated, async (req, res) => {
+    let reviewId = req.params.reviewId;
+    try {
+      reviewId = checkStr(reviewId, "reviewId");
+      if (!ObjectId.isValid(reviewId)) throw "the reviewId is not valid";
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+    try {
+      const deleteInfo = await removeByFlaggedTimes(reviewId);
+      if (!deleteInfo) throw "deleting failed";
+      return res.render("flaggedReviews");
+    } catch (e) {
+      return res.status(404).json({ error: e });
+    }
+  });
